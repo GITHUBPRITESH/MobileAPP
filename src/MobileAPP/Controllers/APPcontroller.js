a798280e9fd8d52406468e23134448fc48f3774a
Pritesh Patel <pritespatel@gmail.com>
2017-05-16 15:59:05 +0530

    Push ALL

---
+++ src/MobileAPP/Controllers/APPcontroller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('APPcontroller', APPcontroller);
+
+    APPcontroller.$inject = ['$location']; 
+
+    function APPcontroller($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'APPcontroller';
+
+        activate();
+
+        function activate() { }
+    }
+})();
